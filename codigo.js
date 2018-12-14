0674dfa015189a0a3816dfa7464dc175848e2b45
pedro846 <45462791+pedro846@users.noreply.github.com>
2018-12-14 16:52:10 -0600

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,55 @@
+var articulo =  prompt("Para realizar su compra ingrese el codigo")
+ if(articulo=="Blon123"){
+    alert("Su compra ha sido realizada Usted compro un Balon");
+ }
+ else if(articulo=="Rqt321"){
+     alert("Su compra ha sido realizada compro una Raqueta ");
+ }
+else if(articulo=="Gts567"){
+    alert("Su compra ha sido realizada compro un par de Guantes de futbol");
+}
+else if(articulo=="Clg890"){
+    alert("Su compra ha sido realizada compro Camiseta");
+}
+else if(articulo=="Ptb098"){
+    alert("Su compra ha sido realizada un Protector de boxear");
+}
+else if(articulo=="Bts654"){
+    alert("Su compra ha sido realizada un par de Botines de soccer");
+}
+else if(articulo=="Pts123"){
+    alert("Su compra ha sido realizada un par de Patines");
+}  
+else if(articulo=="Btb321"){
+    alert("Su compra ha sido realizada un Traje"); 
+}
+else if(articulo=="Bll806"){
+    alert("Su compra ha sido realizada un Balon de boley ball");
+}
+else if(articulo=="PTB321"){
+    alert("Su compra ha sido realizada Protector bucal")
+}
+else if(articulo=="Beis123"){
+    alert("Su compra ha sido realizada Bat de beisboll")
+}
+else if(articulo=="Gdb678"){
+    alert("Su compra ha sido realizada Guante de beisboll")
+}
+else if(articulo=="Chp098"){
+    alert("Su compra ha sido realizada Chimpinillera")
+}
+else if(articulo=="Tms674"){
+    alert("Su compra ha sido realizada Termos para agua")
+}
+else if(articulo=="Snw012"){
+    alert("Su compra ha sido realizada Tabla de snowboard")
+}
+else if(articulo=="Bbll836"){
+    alert("Su compra ha sido realizada Botas de beisboll")
+}
+else if(articulo=="Grr098"){
+    alert("Su compra ha sido realizada Gorras de beisboll")
+}
+
+console.log(Articulo);
+
